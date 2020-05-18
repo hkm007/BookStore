ff1ca22f108eb326d9e2202f7405bde1bc930282
hkm007 <hkm0072001@gmail.com>
2020-05-18 20:10:07 +0530

    auth modified

--- client/views.py
+++ client/views.py
@@ -8,11 +8,21 @@ def signup(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
-        password = request.POST.get('password')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
 
-        User.objects.create_user(username, email, password)
-        messages.success(request, 'Account Successfully created! Now you can login.')
+        if password1 != password2:
+            messages.warning(request, 'Invalid Credentials. Please try again.')
+        else:
+            myuser = User.objects.create_user(username, email, password1)
+            myuser.first_name = fname
+            myuser.last_name = lname
+            myuser.save()
+            messages.success(request, 'Account Successfully created! Now you can login.')
         return redirect('home')
+        
     else:
         return HttpResponse('Error: 404 Not Found')
 
@@ -27,7 +37,7 @@ def handlelogin(request):
             login(request, user)
             messages.success(request, 'Welcome! You are successfully logged in.')
         else:
-            messages.warning(request, 'Invalid Credentials. Try again')
+            messages.warning(request, 'Invalid Credentials. Please try again.')
             
         return redirect('home')
     else:
