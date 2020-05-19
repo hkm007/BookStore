288bd0711c4d6aa5d504f727893b8dd6b58f246e
Deepraj <bdeeprajrkm1@gmail.com>
2020-05-20 04:13:03 +0530

    resolved conflicts I guess

--- server/settings.py
+++ server/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hostusermail'
-EMAIL_HOST_PASSWORD = 'hostPassword'
+EMAIL_HOST_USER = 'bdeeprajrkm1@gmail.com'
+EMAIL_HOST_PASSWORD = 'DEEPab@37211109'
