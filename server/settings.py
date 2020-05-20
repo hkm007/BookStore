f13081f976b29de77270d529f8a53e4e039972f8
hkm007 <hkm0072001@gmail.com>
2020-05-21 01:12:03 +0530

    footer and recaptcha added

--- server/settings.py
+++ server/settings.py
@@ -128,9 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bdeeprajrkm1@gmail.com'
-EMAIL_HOST_PASSWORD = 'DEEPab@37211109'
-
-LOGIN_REDIRECT_URL = 'home'
-
-LOGIN_URL = 'home'
+EMAIL_HOST_USER = 'pingoo.techie@gmail.com'
+EMAIL_HOST_PASSWORD = 'Pingoo@20'
--- store/views.py
+++ store/views.py
@@ -6,6 +6,8 @@ from .models import Book
 from django.core.mail import EmailMessage,send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
+import requests
+import json
 
 def home(request):
     return render(request, 'store/home.html')
@@ -16,24 +18,24 @@ def products(request):
     return render(request, 'store/products.html',params)
 
 def contact(request):
-    params  = {"msg_disp":False}
-    if request.method == 'POST':
-        userName = request.POST.get('UserName','')
-        userEmail = request.POST.get('UserEmail','')
-        userMsg = request.POST.get('UserMessage')
-        template = render_to_string('store/contact_message.html',{'UserName':userName,'UserEmail':userEmail,'UserMsg':userMsg})
-        email = EmailMessage(
-            'Message from '+userName,
-            template,
-            settings.EMAIL_HOST_USER,
-            ['deeprajbaiddya@gmail.com']
-        )
-        email.fail_silently = False
-        email.send()
-        params = {"msg_disp":True,"msg":"Your form has been submitted successfully."}
-        return render(request,'store/contact.html',params)
-    else:
-        return render(request, 'store/contact.html',params)
+    # params  = {"msg_disp":False}
+    # if request.method == 'POST':
+    #     userName = request.POST.get('UserName','')
+    #     userEmail = request.POST.get('UserEmail','')
+    #     userMsg = request.POST.get('UserMessage')
+    #     template = render_to_string('store/contact_message.html',{'UserName':userName,'UserEmail':userEmail,'UserMsg':userMsg})
+    #     email = EmailMessage(
+    #         'Message from '+userName,
+    #         template,
+    #         settings.EMAIL_HOST_USER,
+    #         ['deeprajbaiddya@gmail.com']
+    #     )
+    #     email.fail_silently = False
+    #     email.send()
+    #     params = {"msg_disp":True,"msg":"Your form has been submitted successfully."}
+    #     return render(request,'store/contact.html',params)
+    # else:
+    return render(request, 'store/contact.html')
 
 def handlecontact(request):
     if request.method == 'POST':
@@ -42,20 +44,40 @@ def handlecontact(request):
         message = request.POST.get('message')
         status = request.POST.get('status')
 
-        # deepraj implement email logic here
-        
-        print(qname, email, message, status)  # just for testing
+        clientkey = request.POST.get('g-recaptcha-response')
+        secretkey = "6LcMOvoUAAAAADVKv88IJc9zJzH-5jgO_oFiwRN0"
 
-        messages.success(request, 'Your message has been sent successfully! Our team will contact you soon.')
+        captchaData = {
+            "secret": secretkey,
+            "response": clientkey
+        }
+
+        r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=captchaData)
+        response = json.loads(r.text)
+        verify = response['success']
+
+        if verify:
+            # deepraj implement email logic here
+            template = render_to_string('store/contact_message.html',{'UserName':qname,'UserEmail':email,'UserMsg':message})
+            email = EmailMessage(
+                'Message from '+qname,
+                template,
+                settings.EMAIL_HOST_USER,
+                ['pingoo.techie@gmail.com']
+            )
+            email.fail_silently = False
+            email.send()
+            print(qname, email, message, status)  # just for testing
+            messages.success(request, 'Your message has been sent successfully! Our team will contact you soon.')
+        else:
+            messages.warning(request, 'reCaptcha not verified!')
         return redirect('contact')
     else:
         return HttpResponse('Error: 404 Not Found')
 
-@login_required
 def cart(request):
-    # if request.user.is_authenticated:
-    #     return render(request, 'store/cart.html')
-    # else:
-    #     return HttpResponse('Error: 404 Not Found')
-    return render(request, 'store/cart.html')
+    if request.user.is_authenticated:
+        return render(request, 'store/cart.html')
+    else:
+        return HttpResponse('Error: 404 Not Found')
     
