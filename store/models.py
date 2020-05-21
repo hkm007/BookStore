80edf26fab4b6a9025046dc03f50a4edcf527b9a
Deepraj <bdeeprajrkm1@gmail.com>
2020-05-22 03:37:05 +0530

    fixed all conflicts

--- store/models.py
+++ store/models.py
@@ -16,15 +16,15 @@ class Book(models.Model):
     )
 
     book_id = models.AutoField(primary_key=True)
-    book_name = models.CharField(max_length=100)
-    book_price = models.FloatField(max_length=20)
-    book_author= models.CharField(max_length=100)
-    book_desc= models.CharField(max_length=200)
-    book_status = models.CharField(max_length=20,choices=STATUS)
+    book_name = models.CharField(max_length=30)
+    book_price = models.FloatField(max_length=30)
+    book_author= models.CharField(max_length=30)
+    book_desc= models.CharField(max_length=30)
+    book_status = models.CharField(max_length=30,choices=STATUS)
     book_pic = models.ImageField(upload_to='img/',default="None Selected")
-    book_date = models.DateField(max_length=20)
-    book_class = models.CharField(max_length=20,choices=CLASS)
-    book_category  = models.CharField(max_length=20,choices=CATEGORY)
+    book_date = models.DateField(max_length=30)
+    book_class = models.CharField(max_length=30,choices=CLASS)
+    book_category  = models.CharField(max_length=30,choices=CATEGORY)
 
     def __str__(self):
         return self.book_name
