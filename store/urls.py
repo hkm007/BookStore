60a93ea0f66bdf5363341633e0fe64ccc44ac2ea
Deepraj <bdeeprajrkm1@gmail.com>
2020-05-22 04:02:29 +0530

    changes in Deep_brach added to master

--- store/urls.py
+++ store/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products', views.products, name='products'),
+    path("products/<int:my_id>",views.productView,name= "productView"),
+    path('books/filter', views.handlefilter, name='books-filter'),
     path('contact', views.contact, name='contact'),
     path('cart', views.cart, name='cart'),
+    path('addbook/', views.addBook, name='addBook'),
     path('contact/post', views.handlecontact, name='contact-post'),
-    path('books/filter', views.handlefilter, name='books-filter'),
 ]
