a67b7652cfeb88548fc8607f83c6becc381582ad
hkm007 <hkm0072001@gmail.com>
2020-05-21 18:56:43 +0530

    signup bug solved and filter logic created

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('contact', views.contact, name='contact'),
     path('cart', views.cart, name='cart'),
     path('contact/post', views.handlecontact, name='contact-post'),
+    path('books/filter', views.handlefilter, name='books-filter'),
 ]
