009c57cfb591c824ab5525b6aaee08ab6ad8e08a
Deepraj <bdeeprajrkm1@gmail.com>
2020-05-22 03:18:14 +0530

    added product view page and fixed some UI bugs

--- client/views.py
+++ client/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+import requests
+import json
 
 def signup(request):
     if request.method == 'POST':
@@ -10,8 +12,29 @@ def signup(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        User.objects.create_user(username, email, password)
-        messages.success(request, 'Account Successfully created! Now you can login.')
+        clientkey = request.POST.get('g-recaptcha-response')
+        secretkey = "6LcMOvoUAAAAADVKv88IJc9zJzH-5jgO_oFiwRN0"
+
+        captchaData = {
+            "secret": secretkey,
+            "response": clientkey
+        }
+
+        r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=captchaData)
+        response = json.loads(r.text)
+        verify = response['success']
+
+        if verify:
+            # logic for same username 
+            if User.objects.filter(username=username).exists():
+                messages.warning(request, 'User with this username already exists!')
+            else:
+                User.objects.create_user(username, email, password)
+                messages.success(request, 'Account Successfully created! Now you can login.')
+            
+        else:
+            messages.warning(request, 'reCaptcha not verfied!')
+        
         return redirect('home')
     else:
         return HttpResponse('Error: 404 Not Found')
@@ -21,13 +44,30 @@ def handlelogin(request):
         loginusername = request.POST.get('loginusername')
         loginpassword = request.POST.get('loginpassword')
 
-        user = authenticate(username = loginusername, password = loginpassword)
+        clientkey = request.POST.get('g-recaptcha-response')
+        secretkey = "6LcMOvoUAAAAADVKv88IJc9zJzH-5jgO_oFiwRN0"
 
-        if user is not None:
-            login(request, user)
-            messages.success(request, 'Welcome! You are successfully logged in.')
+        captchaData = {
+            "secret": secretkey,
+            "response": clientkey
+        }
+
+        r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=captchaData)
+        response = json.loads(r.text)
+        verify = response['success']
+
+        if verify:
+            user = authenticate(username = loginusername, password = loginpassword)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Welcome! You are successfully logged in.')
+                return redirect('products')
+            else:
+                messages.warning(request, 'Invalid Credentials. Try again')
+
         else:
-            messages.warning(request, 'Invalid Credentials. Try again')
+            messages.warning(request, 'reCaptcha not verified!')
             
         return redirect('home')
     else:
--- store/views.py
+++ store/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Book
 from django.core.mail import EmailMessage,send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
+import requests
+import json
+from .book_form import BookForm
 
 def home(request):
     return render(request, 'store/home.html')
@@ -14,25 +18,20 @@ def products(request):
     params = {'Book':data}
     return render(request, 'store/products.html',params)
 
-def contact(request):
-    params  = {"msg_disp":False}
+def productView(request,my_id):
+    data = Book.objects.filter(book_id = my_id)
+    return render(request,'store/productView.html',{"product":data[0]})
+
+def handlefilter(request):
     if request.method == 'POST':
-        userName = request.POST.get('UserName','')
-        userEmail = request.POST.get('UserEmail','')
-        userMsg = request.POST.get('UserMessage')
-        template = render_to_string('store/contact_message.html',{'UserName':userName,'UserEmail':userEmail,'UserMsg':userMsg})
-        email = EmailMessage(
-            'Message from '+userName,
-            template,
-            settings.EMAIL_HOST_USER,
-            ['deeprajbaiddya@gmail.com']
-        )
-        email.fail_silently = False
-        email.send()
-        params = {"msg_disp":True,"msg":"Your form has been submitted successfully."}
-        return render(request,'store/contact.html',params)
+        branchSelect = request.POST.get('branchSelect')
+        print(branchSelect)
+        return redirect('products')
     else:
-        return render(request, 'store/contact.html',params)
+        return HttpResponse('Error: 404 Not Found')
+
+def contact(request):
+    return render(request, 'store/contact.html')
 
 def handlecontact(request):
     if request.method == 'POST':
@@ -41,11 +40,59 @@ def handlecontact(request):
         message = request.POST.get('message')
         status = request.POST.get('status')
 
-        # deepraj implement email logic here
-        
-        print(qname, email, message, status)  # just for testing
+        clientkey = request.POST.get('g-recaptcha-response')
+        secretkey = "6LcMOvoUAAAAADVKv88IJc9zJzH-5jgO_oFiwRN0"
 
-        messages.success(request, 'Your message has been sent successfully! Our team will contact you soon.')
+        captchaData = {
+            "secret": secretkey,
+            "response": clientkey
+        }
+
+        r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=captchaData)
+        response = json.loads(r.text)
+        verify = response['success']
+
+        if verify:
+            # deepraj implement email logic here
+            template = render_to_string('store/contact_message.html',{'UserName':qname,'UserEmail':email,'UserMsg':message})
+            email = EmailMessage(
+                'Message from '+qname,
+                template,
+                settings.EMAIL_HOST_USER,
+                ['pingoo.techie@gmail.com']
+            )
+            email.fail_silently = False
+            email.send()
+            print(qname, email, message, status)  # just for testing
+            messages.success(request, 'Your message has been sent successfully! Our team will contact you soon.')
+        else:
+            messages.warning(request, 'reCaptcha not verified!')
         return redirect('contact')
     else:
         return HttpResponse('Error: 404 Not Found')
+
+def cart(request):
+    if request.user.is_authenticated:
+        return render(request, 'store/cart.html')
+    else:
+        return HttpResponse('Error: 404 Not Found')
+
+
+###################### This is for admin only ########################
+
+def addBook(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            print("saved successfully--------------------------------------------------------------------")
+            msg = "The form has been submitted successfully"
+            params = {"form":form,"msg":msg}
+            return render(request,'store/addBook.html',params)
+        else:
+            return HttpResponseNotFound("OOps Page not Found")
+    else:
+        form = BookForm()
+        msg = False
+        params = {"form":form,"msg":msg}
+        return render(request,'store/addBook.html',params)
